a471559e2f7d3940806d78ab9c7f6c1ad90f4677
TieuBinh123 <107189807+TieuBinh123@users.noreply.github.com>
2022-06-10 16:01:32 +0700

    Add files via upload

---
+++ sp.js
@@ -0,0 +1,24 @@
+var images = [
+    "./img/sp1.jpg",
+    "./img/sp3.jpg",
+    "./img/sp4.jpg",
+];
+var num = 0;
+
+function next() {
+    var slider = document.getElementById("slider");
+    num++;
+    if(num >= images.length) {
+        num = 0;
+    }
+    slider.src = images[num];
+}
+
+function prev() {
+    var slider = document.getElementById("slider");
+    num--;
+    if(num < 0) {
+        num = images.length-1;
+    }
+    slider.src = images[num];
+}
